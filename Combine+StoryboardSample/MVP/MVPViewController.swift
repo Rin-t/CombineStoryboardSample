2c4ec83a132f81a0b1386e25e4ddf0bfdbffb60c
Rin-t <rin.t.4921@gmail.com>
2023-05-20 16:34:09 +0900

    MVPサンプル完成

--- Combine+StoryboardSample/MVP/MVPViewController.swift
+++ Combine+StoryboardSample/MVP/MVPViewController.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 final class MVPViewController: UIViewController {
-    
+
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction private func tappedButton(_ sender: Any) {
+        presenter.tappedButton()
+    }
+
+    private var presenter: PresenterInput!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = Presenter(view: self)
+    }
+
+}
+
+extension MVPViewController: PresenterOutput {
+
+    func showResult(num: Int) {
+        resultLabel.text = String(num)
+    }
 }
--- Combine+StoryboardSample/MVP/Presenter.swift
+++ Combine+StoryboardSample/MVP/Presenter.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol PresenterInput: AnyObject{
+    func tappedButton()
+}
+
+protocol PresenterOutput: AnyObject {
+    func showResult(num: Int)
+}
+
+
+final class Presenter: PresenterInput {
+
+    private unowned let view: PresenterOutput
+    private let generator = RandomNumberGenerator()
+
+    init(view: PresenterOutput) {
+        self.view = view
+    }
+
+    func tappedButton() {
+        let number = generator.getNumber()
+        view.showResult(num: number)
+    }
+}
