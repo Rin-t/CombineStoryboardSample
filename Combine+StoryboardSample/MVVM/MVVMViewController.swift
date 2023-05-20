0825c88267b7271ca977e2e5d6794063e09e7a82
Rin-t <rin.t.4921@gmail.com>
2023-05-20 17:21:02 +0900

    ViewModel verを作成

--- Combine+StoryboardSample/MVVM/MVVMViewController.swift
+++ Combine+StoryboardSample/MVVM/MVVMViewController.swift
@@ -6,8 +6,30 @@
 //
 
 import UIKit
+import Combine
 
 final class MVVMViewController: UIViewController {
 
+
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction private func tappedButton(_ sender: Any) {
+        viewModel.input.tappedButton()
+    }
+
+    private var viewModel = ViewModel()
+    private var cancellables = Set<AnyCancellable>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel.output.subject
+            .sink { completion in
+                print("completion")
+            } receiveValue: { value in
+                self.resultLabel.text = String(value)
+            }
+            .store(in: &cancellables)
+    }
 }
 
--- Combine+StoryboardSample/MVVM/ViewModel.swift
+++ Combine+StoryboardSample/MVVM/ViewModel.swift
@@ -5,4 +5,34 @@
 //  Created by Rin on 2023/05/20.
 //
 
-import Foundation
+import Combine
+
+protocol ViewModelInput {
+    func tappedButton()
+}
+
+protocol ViewModelOutput {
+    var subject: PassthroughSubject<Int, Never> { get }
+}
+
+protocol ViewModelType {
+    var input: ViewModelInput { get }
+    var output: ViewModelOutput { get }
+}
+
+final class ViewModel: ViewModelInput, ViewModelOutput {
+
+    let subject = PassthroughSubject<Int, Never>()
+    let generator = RandomNumberGenerator()
+
+    func tappedButton() {
+        let number = generator.getNumber()
+        print(number)
+        subject.send(number)
+    }
+}
+
+extension ViewModel: ViewModelType {
+    var input: ViewModelInput { return self }
+    var output: ViewModelOutput { return self }
+}
